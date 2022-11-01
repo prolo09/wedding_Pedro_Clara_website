5cc19ced3acba5b9d8aaeed3c089d2b4293c8b5a
Francisco Power <franciscopower66@gmail.com>
2022-11-01 19:19:59 +0000

    popup cards

---
+++ assets/js/buttons.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav(id) {
+    document.getElementById(id).style.height = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav(id) {
+    document.getElementById(id).style.height = "0%";
+  }
